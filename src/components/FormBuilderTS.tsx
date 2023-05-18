78db96b48dacca67c17865edfb42fa8f57c67ea7
AmmarMaq5ood <103896064+AmmarMaq5ood@users.noreply.github.com>
2023-05-18 11:13:20 +0500

    added a new element

--- src/components/FormBuilderTS.tsx
+++ src/components/FormBuilderTS.tsx
@@ -3,6 +3,9 @@ import FormAddElements from "./FormAddElementsTS";
 import FormElements from "./FormElementsTS";
 import FormPreview from "./FormPreviewTS";
 
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 import "./FormBuilder.css";
 
 type FormElementProps = {
@@ -20,18 +23,6 @@ type FormElement = {
 const FormBuilderTS = () => {
   const [elements, setElements] = useState<FormElement[]>([]);
   const [selectedElement, setSelectedElement] = useState<string>("");
-
-  // const handleAddElement = () => {
-  //   let newProps: FormElementProps = { name: "", placeholder: "" };
-  //   console.log("In handleAddElement selectedElement is : ", selectedElement);
-  //   if (selectedElement === "select") {
-  //     newProps.options = [];
-  //   }
-  //   console.log(newProps.options);
-  //   const newElement: FormElement = { type: selectedElement, props: newProps };
-  //   setElements([...elements, newElement]);
-  //   console.log(elements);
-  // };
 
   const handleAddElement = () => {
     let newProps: FormElementProps = {
@@ -83,43 +74,54 @@ const FormBuilderTS = () => {
     }
   };
 
+  const handleMoveElement = (dragIndex: number, hoverIndex: number) => {
+    const dragElement = elements[dragIndex]
+    const newElements = [...elements]
+    newElements.splice(dragIndex, 1)
+    newElements.splice(hoverIndex, 0, dragElement)
+    setElements(newElements)
+  }
+
   return (
-    <div className="formbuilder__container">
-      <div className="formbuilder__header">
-        <h1
-          style={{
-            textAlign: "center",
-            borderBottom: "3px solid black",
-            marginBottom: 20,
-          }}
-        >
-          Form Builder
-        </h1>
-      </div>
-      <div className="formbuilder__body">
-        <div className="formbuilder__form-elements">
-          <FormAddElements
-            selectedElement={selectedElement}
-            setSelectedElement={setSelectedElement}
-            handleAddElement={handleAddElement}
-          />
-          {elements?.map((element, index) => (
-            <FormElements
-              key={index}
-              element={element}
-              index={index}
-              handlePropsChange={handlePropsChange}
-              handleRemoveElement={handleRemoveElement}
-              handleRemoveOption={handleRemoveOption}
+    <DndProvider backend={HTML5Backend}>
+      <div className="formbuilder__container">
+        <div className="formbuilder__header">
+          <h1
+            style={{
+              textAlign: "center",
+              borderBottom: "3px solid black",
+              marginBottom: 20,
+            }}
+          >
+            Form Builder
+          </h1>
+        </div>
+        <div className="formbuilder__body">
+          <div className="formbuilder__form-elements">
+            <FormAddElements
+              selectedElement={selectedElement}
+              setSelectedElement={setSelectedElement}
+              handleAddElement={handleAddElement}
             />
-          ))}
-        </div>
-        <div className="formbuilder__preview">
-          <h2>Preview</h2>
-          <FormPreview elements={elements} handleSubmit={handleSubmit} />
+            {elements?.map((element, index) => (
+              <FormElements
+                key={index}
+                element={element}
+                index={index}
+                handlePropsChange={handlePropsChange}
+                handleRemoveElement={handleRemoveElement}
+                handleRemoveOption={handleRemoveOption}
+                handleMoveElement={handleMoveElement}
+              />
+            ))}
+          </div>
+          <div className="formbuilder__preview">
+            <h2>Preview</h2>
+            <FormPreview elements={elements} handleSubmit={handleSubmit} />
+          </div>
         </div>
       </div>
-    </div>
+    </DndProvider>
   );
 };
 
